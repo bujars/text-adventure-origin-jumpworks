895b7c9897485969130abbffcbcf95f6b2978ae0
Sean <instantiateseankim@gmail.com>
2016-10-30 23:12:19 -0400

    Seki's First Location Implementation

---
+++ LocationSeki1.java
@@ -0,0 +1,11 @@
+public class LocationSeki1 implements Location{
+    
+    public String getName(){
+	return "";
+    }
+
+    public String enter(Player p) throws InterruptedExcaption{
+	return "";
+    }
+}
+
