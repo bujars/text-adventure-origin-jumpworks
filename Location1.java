2a5c2b1e16b1061b06dfb93321f4841fb8ffa581
orangeLibA <orangeLibA@LibTechCtr-iMac-2.local>
2016-11-07 09:54:50 -0500

    Started kingdom

---
+++ Location1.java
@@ -0,0 +1,14 @@
+public class Location1 implements Location
+{
+    public String getName()
+    {
+	return "Location:";
+    }
+
+    public String enter(Player p) throws InterruptedException
+    {
+	return "";
+    }
+
+
+}
