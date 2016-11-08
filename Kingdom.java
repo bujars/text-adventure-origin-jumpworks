ee1112fbee184cf46bca3f00d665c8f0c669aa7d
MARIEDIAOUNE <maried9816@afsenyc.org>
2016-11-08 17:08:36 -0500

    Update and rename Location1.java to Kingdom.java

---
+++ Kingdom.java
@@ -0,0 +1,68 @@
+public class Kingdom implements Location
+{
+    public String getName()
+    {
+	return "Location:";
+    }
+
+    public String enter(Player p) throws InterruptedException
+    {
+    	Scanner sc = new Scanner(System.in);
+	System.out.println("You have leveled up to Kingdom");
+	Thread.sleep(1000);
+	System.out.println();
+	Thread.sleep();
+	System.out.println();
+	Thread.sleep();
+	System.out.println();
+	Thread.sleep();
+	System.out.println();
+	Thread.sleep();
+	Thread.sleep();
+	System.out.println();
+	Thread.sleep();
+	System.out.println();
+	
+	if(decideYesOrNo(p).equals("y"))
+	{
+		System.out.println();
+		Thread.sleep();
+		System.out.println();
+		System.out.print();
+		sc.nextLine();
+		Thread.sleep();
+	}
+	else
+	{
+		System.out.println();
+		Thread.sleep();
+		System.out.println();
+		Thread.sleep();
+		System.out.print();
+		sc.nextLine();
+		Thread.sleep();
+	}
+	return "";
+	
+    }
+    public boolean askYesOrNo(String question, int wait) throws InterruptedException
+    {
+       Scanner sc = new Scanner(System.in);
+       String input = "";
+       System.out.println(question);
+       Thread.sleep(wait);
+       System.out.print("**\n**[y] TO SAY YES\n"+
+                            "**[n] TO SAY NO\n**\n> ");
+       while (input == "")
+       {
+           input = sc.next();//gets user input                                                                                                                                   
+           if (!(input.equals("y")||input.equals("n"))){
+               input = "";
+               System.out.println("That isn't an option try again");
+           }
+       }
+       return input.equals("y");
+   }
+
+
+}
