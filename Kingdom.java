4eb3fa343c2c17323af4afa972384c71373381ef
MARIEDIAOUNE <maried9816@afsenyc.org>
2016-11-08 22:52:50 -0500

    Update Kingdom.java

--- Kingdom.java
+++ Kingdom.java
@@ -10,37 +10,37 @@ public class Kingdom implements Location
     	Scanner sc = new Scanner(System.in);
 	System.out.println("You have leveled up to Kingdom");
 	Thread.sleep(1000);
+	System.out.println("You're Kingdom has reached the top");
+	Thread.sleep(1000);
 	System.out.println();
-	Thread.sleep();
+	Thread.sleep(1000);
 	System.out.println();
-	Thread.sleep();
+	Thread.sleep(1000);
 	System.out.println();
-	Thread.sleep();
+	Thread.sleep(1000);
+	Thread.sleep(1000);
 	System.out.println();
-	Thread.sleep();
-	Thread.sleep();
-	System.out.println();
-	Thread.sleep();
+	Thread.sleep(1000);
 	System.out.println();
 	
 	if(decideYesOrNo(p).equals("y"))
 	{
 		System.out.println();
-		Thread.sleep();
+		Thread.sleep(1000);
 		System.out.println();
 		System.out.print();
 		sc.nextLine();
-		Thread.sleep();
+		Thread.sleep(1000);
 	}
 	else
 	{
 		System.out.println();
-		Thread.sleep();
+		Thread.sleep(1000);
 		System.out.println();
-		Thread.sleep();
+		Thread.sleep(1000);
 		System.out.print();
 		sc.nextLine();
-		Thread.sleep();
+		Thread.sleep(1000);
 	}
 	return "";
 	
