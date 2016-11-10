8ee8ea9ffbf9f5de52ad7c041a15a5fb9845d2e2
Bujar Sefa <BujarSefa@Bujars-MacBook-Air.local>
2016-11-10 00:43:18 -0500

    Basically City main method

--- AdventureForSoftwareEngineering.java
+++ AdventureForSoftwareEngineering.java
@@ -12,8 +12,7 @@ public class AdventureForSoftwareEngineering implements Game{
     @Override
     public List<Location> getLocations(){
 	List<Location> levels = new ArrayList<Location>();
-	levels.add(new ApartmentWakeUp());
-	levels.add(new SubwayPlatformMorning());
+	levels.add(new City());
 	return levels;
     }
 
