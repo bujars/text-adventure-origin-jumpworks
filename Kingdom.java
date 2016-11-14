1929c2691248c02c8fdeed419ba78f6bab931bb3
MARIEDIAOUNE <maried9816@afsenyc.org>
2016-11-14 09:53:26 -0500

    Update Kingdom.java
    Added the story line

--- Kingdom.java
+++ Kingdom.java
@@ -1,85 +1,91 @@
 import java.util.Scanner;
-
 public class Kingdom extends AdvancedLocation
+
 {
-    public String getName()
+  public String getName()
     {
-	return "Location:";
+	return "Kingdom";
     }
 
     public String enter(Player p) throws InterruptedException
     {
+
     	Scanner sc = new Scanner(System.in);
-	String input = "";
 	String question = "";
+	String input = "";
 	
-	betterPrint("You have leveled up to Kingdom");
-	nextLine();
-	betterPrint();
-	nextLine();
-	betterPrint();
-	nextLine();
-	betterPrint();
-	nextLine();
-	betterPrint();
-	nextLine();
-	betterPrint();
-	nextLine();
-	System.out.println();
-	Thread.sleep(1000);
-	System.out.println("You're Kingdom has reached the top");
-	Thread.sleep(1000);
-	System.out.println();
-	Thread.sleep(1000);
-	System.out.println();
-	Thread.sleep(1000);
-	System.out.println();
-	Thread.sleep(1000);
-	Thread.sleep(1000);
-	System.out.println();
-	Thread.sleep(1000);
-	System.out.println();
 	
-	if(decideYesOrNo(p).equals("y"))
+	betterPrint("--You have leveled up to Kingdom--");
+	nextLine();
+	betterPrint("Gained 1000 Health, 1000 Money, and 1000 Population!");
+	nextLine();
+	betterPrint("Princess Bubblegum: My King! Congrats!");
+	betterPrint("Princess Bubblegum: Ou has never had more faith in you more than no!");
+	nextLine();
+	betterPrint("-Door Knocks-");
+	betterPrint("Princess Bubblegum: Your Highness, Peppermint Butler has come to see you.");
+	betterPrint("Princess Bubblegum: I will let Peppermint Butler in at once!");
+	nextLine();
+	betterPrint("Peppermint Butler: My King, demons from the underworld are surfacing and coming to take over Ou!");
+	betterPrint("Peppermint Butler: But we can defeat them before they reach");
+	question = "Peppermint Butler: Please protect the citzens of OU by giving me 1000 gold so I can bulid a wall.";
+	
+	if(askYesOrNo(question))
 	{
-		System.out.println();
-		Thread.sleep(1000);
-		System.out.println();
-		System.out.print();
-		sc.nextLine();
-		Thread.sleep(1000);
+		
+		betterPrint("-1000 Gold, +1000 Happiness.");
+		nextLine();
+		betterPrint("**!**");
+		betterPrint("Peppermint Butler: Oh My King! Thank You for Everything! The demons shall not win!");
+		nextLine();
 	}
 	else
 	{
-		System.out.println();
-		Thread.sleep(1000);
-		System.out.println();
-		Thread.sleep(1000);
-		System.out.print();
-		sc.nextLine();
-		Thread.sleep(1000);
+		
+		betterPrint("--The demons have attacked your Kingdom!--");
+		nextLine();
+		betterPrint("-1000 Gold, -1000 Happiness, -1000 Population.");
+		nextLine();
+		betterPrint("Peppermint Butler: I will eat your flesh!");
+		nextLine();
 	}
-	return "";
+	if(askYesOrNo(question))
+	{
+		
+		betterPrint("-1000 Gold, +1000 Happiness.");
+		nextLine();
+		betterPrint("**!**");
+		betterPrint("Peppermint Butler: Oh My King! Thank You for Everything! The demons shall not win!");
+		nextLine();
+	}
+	else
+	{
+		
+		betterPrint("--The demons have attacked your Kingdom!--");
+		nextLine();
+		betterPrint("-1000 Gold, -1000 Happiness, -1000 Population.");
+		nextLine();
+		betterPrint("Peppermint Butler: I will eat your flesh!");
+		nextLine();
+	}
 	
+	return "Game Over";
+
+	
+
     }
-    public boolean askYesOrNo(String question, int wait) throws InterruptedException
-    {
-       Scanner sc = new Scanner(System.in);
-       String input = "";
-       System.out.println(question);
-       Thread.sleep(wait);
-       System.out.print("**\n**[y] TO SAY YES\n"+
-                            "**[n] TO SAY NO\n**\n> ");
-       while (input == "")
-       {
-           input = sc.next();//gets user input                                                                                                                                   
-           if (!(input.equals("y")||input.equals("n"))){
-               input = "";
-               System.out.println("That isn't an option try again");
-           }
-       }
-       return input.equals("y");
-   }
 
 
+ public static void main(String[] args){
+	Kingdom k = new Kingdom();
+	try 
+	{
+	    k.enter(new Teacher());
+	    System.out.println("Kingdom Class");
+	} 
+	catch (InterruptedException e)
+	{
+	    System.out.println("Something Broke");
+	}
+    }
 }
