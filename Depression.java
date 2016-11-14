0444722b39411a89ba674ed5aea111da342084f1
bujars <bujars0698@afsenyc.org>
2016-11-13 20:08:35 -0500

    Update Depression.java

--- Depression.java
+++ Depression.java
@@ -30,17 +30,19 @@ public class Depression extends AdvancedLocation
 	System.out.println("Cake: My King to be the great kingdom of OU, we need security. \n Please protect the citzens of OU by giving me 100 gold to bulid a wall.");
 	*/
 	
-	betterPrint("**Your Kingdom has failed. You Kingdon is in a state of Depression!**");
+	betterPrint("--Your Kingdom has failed. You Kingdom is in a state of Depression!--");
 	nextLine();
-	betterPrint("**Lost 1000 Health, 1000 Money, and 1000 Population!");
+	betterPrint("Lost 1000 Health, 1000 Money, and 1000 Population!");
 	nextLine();
 	betterPrint("Princess Bubblegum: My King! What did you do to us!");
 	betterPrint("Princess Bubblegum: No one believes in your power anymore! You have destroyed much of us!");
 	nextLine();
+	betterPrint("-Door Knocks-")
 	betterPrint("Princess Bubblegum: Your Highness, Cake has come to see you.");
 	betterPrint("Princess Bubblegum: I will let Cake in at once!");
 	nextLine();
-	betterPrint("Cake: My King, to be the great kingdom of OU, we need security."); 
+	betterPrint("Cake: My King, to be the great kingdom of OU, we need security.");
+	betterPrint("Cake: My idea is to build a wall that will provide jobs and keep the city protected!")
 	question = "Cake: Please protect the citzens of OU by giving me 1000 gold so I can bulid a wall.";
 	
 	if(askYesOrNo(question))
@@ -54,7 +56,7 @@ public class Depression extends AdvancedLocation
 		betterPrint("-1000 Gold, +1000 Happiness.");
 		nextLine();
 		betterPrint("**The Great Wall of OU was Built!**");
-		betterPrint("Cake: Oh My King! Thank You for Everything! Our City will hopefully be saved.");
+		betterPrint("Cake: Oh My King! Thank You for Everything! Our Kingdom will hopefully be saved.");
 		nextLine();
 	}
 	else
@@ -66,13 +68,16 @@ public class Depression extends AdvancedLocation
 		System.out.print("\n**\n**[enter] TO CONTINUE \n**\n>");
 		sc.nextLine();
 		Thread.sleep(1000);*/
-		betterPrint("**OZ has attacked your city!**");
+		betterPrint("--OZ has attacked your city!--");
 		nextLine();
 		betterPrint("-1000 Gold, -1000 Happiness, -1000 Population.");
 		nextLine();
 		betterPrint("Cake: I told you my king! Our people are ruined!");
 		nextLine();
 	}
+	betterPrint("Princess Bubblegum: My King, what have you done?");
+	betterPrint("Princess Bubblegum: Every decision you have made has led to our downfall.");
+	betterPrint("Princess Bubblegum: Please my king, from now on, think of the people!");
 	return "Game Over";
 
 	
