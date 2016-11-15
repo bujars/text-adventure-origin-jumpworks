293da6974da291357903e4e83b221c7e6e9884f6
Bujar Sefa <BujarSefa@Bujars-MacBook-Air.local>
2016-11-15 09:54:57 -0500

    Created King

---
+++ King.java
@@ -0,0 +1,35 @@
+/**
+ * Sample player for school based text adventure game.
+ * @author Sean Stern
+ * @version 1.0
+ */
+public class King implements Player{
+    
+    private int health;
+    
+    public King()
+    {
+	health = 100;
+    }
+
+    @Override
+    public int getHealth()
+    {
+	return health;
+    }
+
+    @Override
+	public void changeHealth(int delta){
+	if(!(delta < 0 && delta > health))
+	{
+	    health -= delta;
+	}
+    }
+
+    @Override
+	public Inventory getInventory(){
+        // Teachers have nothing but their education to
+        // make it through school
+        return null; 
+    }
+}
