4b3408a4239a36ca7845ea283d94ac6568db1e95
MARIEDIAOUNE <maried9816@afsenyc.org>
2016-11-14 22:54:02 -0500

    Update Kingdom.java
    Finished this class

--- Kingdom.java
+++ Kingdom.java
@@ -27,15 +27,15 @@ public class Kingdom extends AdvancedLocation
 	betterPrint("Princess Bubblegum: I will let Peppermint Butler in at once!");
 	nextLine();
 	betterPrint("Peppermint Butler: My King, demons from the underworld are surfacing and coming to take over Ou!");
-	betterPrint("Peppermint Butler: But we can defeat them before they reach");
-	question = "Peppermint Butler: Please protect the citzens of OU by giving me 1000 gold so I can bulid a wall.";
+	betterPrint("Peppermint Butler: But we can defeat them before they reach the kingdom");
+	question = "Peppermint Butler: Please give me an army and weapons";
 	
 	if(askYesOrNo(question))
 	{
 		
 		betterPrint("-1000 Gold, +1000 Happiness.");
 		nextLine();
-		betterPrint("**!**");
+		betterPrint("**An army has created and equipted with top the line weapons**");
 		betterPrint("Peppermint Butler: Oh My King! Thank You for Everything! The demons shall not win!");
 		nextLine();
 	}
@@ -49,27 +49,6 @@ public class Kingdom extends AdvancedLocation
 		betterPrint("Peppermint Butler: I will eat your flesh!");
 		nextLine();
 	}
-	//This is doing the same this as above. I just commented it out so it doesn't mess up the code. 
-	/*
-	if(askYesOrNo(question))
-	{
-		
-		betterPrint("-1000 Gold, +1000 Happiness.");
-		nextLine();
-		betterPrint("**!**");
-		betterPrint("Peppermint Butler: Oh My King! Thank You for Everything! The demons shall not win!");
-		nextLine();
-	}
-	else
-	{
-		
-		betterPrint("--The demons have attacked your Kingdom!--");
-		nextLine();
-		betterPrint("-1000 Gold, -1000 Happiness, -1000 Population.");
-		nextLine();
-		betterPrint("Peppermint Butler: I will eat your flesh!");
-		nextLine();
-	}*/
 	
 	return "Game Over";
 
