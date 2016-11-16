d543a6054e08fa01de08ccd9016a077bd13a5cd3
Sean Kim <instantiateseankim@gmail.com>
2016-11-16 07:10:19 -0500

    Added Magic Wand for testting purposes

--- AdventureForSoftwareEngineering.java
+++ AdventureForSoftwareEngineering.java
@@ -12,8 +12,8 @@ public class AdventureForSoftwareEngineering implements Game{
     @Override
     public List<Location> getLocations(){
 	List<Location> levels = new ArrayList<Location>();
-	levels.add(new City());
-	levels.add(new PoorLevel());
+        levels.add(new ApartmentWakeUp());
+	levels.add(new SubwayPlatformMorning());
 	return levels;
     }
 
