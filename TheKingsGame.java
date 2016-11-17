5a2e4eb47a77a9fb6b50c5bda5a8f88c1ef6e62d
Sean Kim <instantiateseankim@gmail.com>
2016-11-17 11:55:18 -0500

    Added custom game class 'TheKingsGame'

---
+++ TheKingsGame.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class TheKingsGame implements Game{
+    @Override
+    public List<Location> getLocations(){
+	List<Location> levels = new ArrayList<Location>();
+        levels.add(new Tutorial());
+	levels.add(new Village());
+	levels.add(new City());
+	levels.add(new PoorLevel());
+	return levels;
+    }
+
+    @Override
+    public Player getPlayer(){
+	return new Teacher();
+    }
+    
+    public static void main(String[] args){
+	
+    }
+}
