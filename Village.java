3aef90eded18a79f482b5fa55bcd259e2e5247f1
Bujar Sefa <BujarSefa@Bujars-MacBook-Air.local>
2016-11-21 00:06:19 -0500

    Fixed Village with money/items/script

--- Village.java
+++ Village.java
@@ -9,22 +9,12 @@ public class Village extends HelperClass implements Location{
 	Scanner sc = new Scanner(System.in);
 	String input = "";
 	String question = "";
-	//question = "Hey can I have a money to make a game?"; 
-	/*question = "Hey can I have a money to make a game?"; 
-	if(askYesOrNo(question)){
-	    betterPrint("Yay thanks!");
-	    betterPrint("\n**\n**Gained 50 happiness\n**\n>");
-	} else {
-	    betterPrint("Damn!");
-	    betterPrint("\n**\n**Gained 50 happiness\n**\n>");
-	}
-	}*/
-	betterPrint("--OKAY, you will start off with a small village to rule.--");
-	//betterPrint("--Your assistant will be Princess Bubblegum.--");
+	betterPrint("-- Okay, you will start off with a small village to rule. --");
 	nextLine();
-	betterPrint("** Starting off with 100 Gold, 100 Health, 100 Happiness **");
-	nextLine();
+	betterPrint("** Starting off with 100 Money, 100 Health, 100 Population  **");
+	p.getInventory().addItem(new Money(100));
 	p.changeHealth(0);
+	nextLine();
 	betterPrint("Princess Bubblegum: Hi, your Highness! With your help, we can grow our tiny village to become the greatest kingdom, to become Kingdom OU.");
 	nextLine();
 	betterPrint("Princess Bubblegum: My King, please always think for the greater good of the people!");
@@ -37,11 +27,12 @@ public class Village extends HelperClass implements Location{
 	betterPrint("Villager: Hello my King!");
 	betterPrint("Villager: I know that you will make us prosper and we will become the greatest kingdom and I want to help.");
 	betterPrint("Villager: I want to open up a tea shop and start selling tea, but sadly I don't have enough money.");
-	question = "Villager: Can you lend me 10 gold pieces?";
+	question = "Villager: Can you lend me 10 Money?";
 	
 	if(askYesOrNo(question))
         {
-	    betterPrint("** -10 Gold. +20 Happiness **");
+	    betterPrint("** -10 Money. +20 Population **");
+	    p.getInventory().getItem("Money").use(10);
 	    nextLine();
 	    betterPrint("Villager: Oh my King! We will soon flourish! Thank You!");
 	    nextLine();
@@ -50,12 +41,13 @@ public class Village extends HelperClass implements Location{
 	    betterPrint("-- Villager Punches King --");
 	    nextLine();
 	    betterPrint("** -20 Health ** ");
+	    p.changeHealth(-20);
 	    nextLine();
 	    betterPrint("Villager: My king! You are already proving yourself not worthy. I am ashamed.");
 	    nextLine();
 	    betterPrint("-- Villager runs away and never comes back. --");
 	}
-	betterPrint("Princess Bubblegum: Wow my king.");
+	betterPrint("Princess Bubblegum: Wow my king. I...I don't know if that was right.");
 	nextLine();
 	betterPrint("-- Three days later. --");
 	nextLine();
@@ -75,20 +67,25 @@ public class Village extends HelperClass implements Location{
 	    {
 		betterPrint("Pirate: Well my king, I am giving it to you anyways.");
 		betterPrint("Pirate: I want you to build the greatest kingdom of all.");
-	}
+		nextLine();
+	    }
 	
 	betterPrint("** Pirate ship received. Treasure Found. **");
 	betterPrint("-- Out of excitement you gained health! --");
-	betterPrint("** +100 Health, +500 Gold, +1000 Population **");
-	nextLine();
+	betterPrint("** +100 Health, +500 Money, +1000 Population **");
 	p.changeHealth(100);
-	return "City";
+	p.getInventory().addItem(new Money(500));
+	nextLine();
+	return "Village";
     }
     
     public static void main(String[] args){
 	Village v = new Village();
+	King k = new King();
+	k.getInventory().addItem(new Money());
 	try{
-	    v.enter(new Teacher());
+	    v.enter(k);
+	    System.out.println(k.getInventory().toString());
 	}
 	catch(InterruptedException e){
 	    System.out.println("Something broke");
