cb0bb8a8dc8095fa36ae7c0872203c57743d6419
Sean Kim <instantiateseankim@gmail.com>
2016-12-09 13:14:09 -0500

    Added PepperMintButler.java again

---
+++ PepperMintButler.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PepperMintButler extends HelperClass implements Location{
+    public String getName(){
+	return "PepperMintButler";
+    }
+    
+    public String enter(Player p) throws InterruptedException{
+	Scanner sc = new Scanner(System.in);
+	String input = "";
+	String question = "";
+	return "next";
+    }
+    
+    public static void main(String[] args){
+	Location t = new PepperMintButler();
+	try{
+	    t.enter(new King());
+	}
+	catch(InterruptedException e){
+	    System.out.println("Something broke");
+	}
+	System.out.println("Tutorial main method");
+    }
+} 
